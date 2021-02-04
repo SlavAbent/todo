be627a79cef4a0ba089045bc82e7b02aea9f7e81
SlavAbent <slavabent2015@gmail.com>
2021-02-04 18:32:10 +0500

    todo-list with react v1

---
+++ src/components/app/app.js
@@ -0,0 +1,149 @@
+import React, {Component} from 'react';
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+
+import './app.css';
+
+export default  class App extends Component  {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {label: 'Going to learn React', important: true, like:false, id: 1},
+                {label: 'That is so good', important: false, like:false, id: 2},
+                {label: 'I need a break...', important: false, like:false, id: 3}
+            ],
+            term: '',
+            filter: 'all'
+        };
+
+        this.deleteItem = this.deleteItem.bind(this) 
+        this.addItem = this.addItem.bind(this)
+
+        this.onToggleImportant = this.onToggleImportant.bind(this)
+        this.onToggleLike = this.onToggleLike.bind(this)
+        this.onUpdateSearch = this.onUpdateSearch.bind(this)
+        this.onFilterSelect = this.onFilterSelect.bind(this)
+
+
+        this.maxId = 4
+    }
+
+    searchPost(items, term){
+        if(term.length === 0){
+            return items
+        }
+        return items.filter(item => {
+            return item.label.indexOf(term) > -1 //в каждом элементе мы будет находить label и внутри label то, что ввел пользователь
+        });
+    }
+
+    deleteItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const newArr = [...data.slice(0, index), ...data.slice(index + 1)]; // до нужного нам элемента и после нужного нам элемента
+            return {
+                data: newArr
+            }
+
+        });
+    }
+
+    addItem(body){
+        const newItem = {
+            label: body, 
+            important: false,
+            id: this.maxId++
+        }
+
+        this.setState(({data}) => {
+            const newArr = [...data, newItem]; // все данные из стейта data и добавляем новый item
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    onToggleImportant(id){
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const old = data[index];
+            const newItem = {...old, important: !old.important};
+            const newArr =  [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    onToggleLike(id){
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id);
+            const old = data[index];
+            const newItem = {...old, like: !old.like};
+            const newArr =  [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    onUpdateSearch(term){
+        this.setState({
+            term
+        })
+    }
+
+    filterPost(items, filter){
+        if(filter === 'like'){
+            return items.filter(item => item.like);
+        } else {
+            return items
+        }
+    }
+
+    onFilterSelect(filter){
+        this.setState({filter})
+    }
+
+    render() {
+        const {data, term, filter} = this.state
+        // console.log(data, term, filter);
+        const liked = this.state.data.filter(item => item.like).length;
+        const allPosts = this.state.data.length;
+
+        const visiblePosts = this.filterPost(this.searchPost(data, term), filter); //видимые посты на основании того, что ввел пользователь
+
+        return (
+            <div className="app">
+                    <AppHeader
+                        liked={liked}
+                        allPosts={allPosts}
+                    />
+                    <div className="search-panel d-flex">
+                        <SearchPanel
+                            onUpdateSearch={this.onUpdateSearch}
+                        />
+                        <PostStatusFilter 
+                            filter={filter}
+                            onFilterSelect={this.onFilterSelect}
+                        />
+                    </div>
+                    <PostList 
+                        posts={visiblePosts}
+                        onDelete={this.deleteItem}
+                        onToggleImportant={this.onToggleImportant}
+                        onToggleLike={this.onToggleLike}
+                    />
+                    <PostAddForm
+                        onAdd={this.addItem}
+                    />
+            </div>
+        )
+    }
+}
